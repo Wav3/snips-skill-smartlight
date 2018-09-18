2c4277f7a2e313df6828efc1de46d3c9027505d1
Janek Kröger <janek.kroeger@ewe.net>
2018-09-18 15:58:04 +0200

    Added all required files

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-snips-skill-smartlight
+# Hassio light action for Snips
+
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/Wav3/snips-skill-smartlight/master/LICENSE.txt)
+
+## Introduction
+
+With this action you are able to control your lights in home assistant via Snips.
+
+## Setup
+
+In order to be able to control your lights in home assistant via Snips, you must enable and configure the following home assistant components:
+- [api](https://www.home-assistant.io/components/api/)
+- [http](https://www.home-assistant.io/components/http/) (required for the component api)
+
+For this action the following configuration variables from home assistant are required and should be saved:
+- api_password (used for the value HTTP_API_PASSWORD in config.ini)
+- server_port (used for the value HTTP_API_PORT in config.ini)
+
+### Installation with Sam
+
+This is the easiest way to install this action.  
+You use [Sam](https://snips.gitbook.io/getting-started/installation) to install this action.  
+`sam install actions -g https://github.com/Wav3/snips-skil-smartlight.git`  
+  
+Sam will ask during the installation for the following values to connect the action with hassio:  
+- HTTP_API_PORT (The port where home assistant is listening (default: 8123))
+- HTTP_API_HOSTNAME (The hostname of the home assistant (default: hassio))
+- HTTP_API_PASSWORD (The password of the home assistant for the api)
+
+
+### Manual installation
+
+This skill requires some python dependencies to work properly, these are listed in the `requirements.txt`. You can use the `setup.sh` script to create a python virtualenv that will be recognized by the skill server and install them in it.
+1. Clone the repository on your pi with `git clone https://github.com/Wav3/snips-skil-smartlight.git`
+2. Run `setup.sh` (it will create a virtualenv, install all dependencies defined in 'requirements.txt' and rename config.ini.default to config.ini)
+3. Provide the required values for HTTP_API_PORT, HTTP_API_HOSTNAME and HTTP_API_PASSWORD in the config.ini
+4. Run `action-LightOff.py`
+5. Run `action-LightOn.py`
+
+## Copyright
+
+This action is provided by Janek Kröger as Open Source software. See [LICENSE.txt](https://raw.githubusercontent.com/Wav3/snips-skill-smartlight/master/LICENSE.txt) for more information.
---
+++ action-LightOff.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+import requests
+import json
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIGURATION_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section: {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intent_message):
+    conf = read_configuration_file(CONFIGURATION_INI)
+    action_wrapper(hermes, intent_message, conf)
+
+def action_wrapper(hermes, intent_message, conf):
+    apiport = conf['secret']['http_api_port']
+    apihost = conf['secret']['http_api_hostname']
+    url = 'http://{}:{}/api/services/light/turn_off'.format(apihost, apiport)
+    header = {
+        "Content-Type": "application/json"
+        "x-ha-access": conf['secret']['http_api_password']
+    }
+    current_session_id = intent_message.session_id # get the current session id
+
+    if len(intent_message.slots.Zimmer) > 0: #turns on a specific light
+        zimmer = intent_message.slots.Zimmer.first().value # extract the value from the slot Zimmer
+        result_sentence = "Licht in Raum {} eingeschaltet.".format(str(zimmer))
+        body = {
+            "entity_id": "light.{}".format(str(zimmer))
+        }
+        json_body = json.dumps(body)
+        request = requests.post(url, data = json_body, headers = header)
+    else: # turns on every known light
+        result_sentence = "Alle Lichter wurden eingeschaltet"
+        request = requests.post(url, headers = header)
+    
+    if request.status_code != 200: # if the action is failed, set the response to failed
+        result_sentence = "Das Lichtanschalten ist fehlgeschlagen"
+        print('Error during enabling light.\nStatuscode: {}\nResponse message: {}'.format(request.status_code, request.content))
+
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("kroegerj:LightOff", subscribe_intent_callback).start()
